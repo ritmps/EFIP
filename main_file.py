652a3e17fc8afe47098b54c8a64a8e8254673e14
Nick Duggan <nkd2840@rit.edu>
2022-02-10 19:44:39 -0500

    bonjour

--- main_file.py
+++ main_file.py
@@ -5,9 +5,7 @@ testing testing 123
 
 
 post github desktop things 
-<<<<<<< HEAD
-hi mason
-=======
+
 
 
 mason testing commit 
@@ -16,4 +14,4 @@ mason testing pushing
 
 
 testing for the jetson 
->>>>>>> 0bd4685425f6d8c0eb87be2000f3c1841cb07e5a
+
