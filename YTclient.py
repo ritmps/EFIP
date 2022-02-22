d3472fb022ef12d4de717ff8482f552173d3d9ea
NickNack08 <89480418+NickNack08@users.noreply.github.com>
2022-02-22 14:50:57 -0500

    Add files via upload

---
+++ YTclient.py
@@ -0,0 +1,31 @@
+import socket
+
+#from YTserver import ADDR
+print("hi")
+HEADER = 64
+PORT = 8080
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+#client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(100).decode(FORMAT))
+
+send("Hello World!")
+input()
+send("Hello Everyone!")
+input()
+send("Hello there!")
+
+send(DISCONNECT_MESSAGE)
