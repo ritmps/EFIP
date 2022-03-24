ecc2fed15693f709bc0ea63d9fca1449de159019
Karla-van-Aardt <89264673+Karla-van-Aardt@users.noreply.github.com>
2022-03-24 14:45:52 -0400

    Update color_segment.py
    Update color picker to see the camera.

--- color_segment.py
+++ color_segment.py
@@ -1,7 +1,38 @@
 import cv2
+from cv2 import VideoCapture
 import imutils  
 import numpy as np  
-cap = cv2.VideoCapture(0) # getting video from webcam
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+print(gstreamer_pipeline(flip_method=0))
+cap = VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+#cap = cv2.VideoCapture(0) # getting video from webcam
 while cap.isOpened():
     ret, img = cap.read()
 
