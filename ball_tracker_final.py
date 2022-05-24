7c4a68bef8a954eae0711490ed0cd1824f6638ae
ramancini04 <bam3869@rit.edu>
2022-05-24 11:31:53 -0400

    Updated display resolution

--- ball_tracker_final.py
+++ ball_tracker_final.py
@@ -17,8 +17,8 @@ def gstreamer_pipeline(
     sensor_id=0,
     capture_width=1920,
     capture_height=1080,
-    display_width=960,
-    display_height=540,
+    display_width=1920,
+    display_height=1080,
     framerate=30,
     flip_method=0,
 ):
