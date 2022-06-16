a20d4c63bbeec27d8a90b1bf91f70552487464aa
NickNack08 <89480418+NickNack08@users.noreply.github.com>
2022-06-16 16:27:18 -0400

    Update move.py

--- move.py
+++ move.py
@@ -1,8 +1,13 @@
 import UnityEngine as ue
-
+x = 1.0
+y = 3.0
+z = 4.0
+vector = ue.Vector3(x, y, z)
 objects = ue.Object.FindObjectsOfType(ue.GameObject)
 for parents in objects:
     if parents.name == 'puck2':
+        parents.transform.position = vector
+
         ue.Debug.Log(parents.name)
-        parents.transform.position += parents.transform.position.up * 10
+        # parents.transform.position += parents.transform.position.up * 10
         ue.Debug.Log(str(parents.transform.position))
