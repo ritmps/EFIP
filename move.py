4253ceea07ff4916aab1bc9925cc8bfc9f82991e
NickNack08 <89480418+NickNack08@users.noreply.github.com>
2022-06-16 16:48:36 -0400

    Update move.py

--- move.py
+++ move.py
@@ -1,14 +1,19 @@
+import random
+import time
 import UnityEngine as ue
-x = 1.0
-y = 3.0
-z = 4.0
-coord = (x, y, z)
-vector = ue.Vector3(coord[0], coord[1], coord[2])
-objects = ue.Object.FindObjectsOfType(ue.GameObject)
-for parents in objects:
-    if parents.name == 'puck2':
-        parents.transform.position = vector
 
-        ue.Debug.Log(parents.name)
-        # parents.transform.position += parents.transform.position.up * 10
-        ue.Debug.Log(str(parents.transform.position))
+count = 0
+while count < 10:
+    x = random.randrange(0, 5, 1)
+    y = random.randrange(0, 5, 1)
+    z = random.randrange(0, 5, 1)
+
+    coord = (x, y, z)
+    vector = ue.Vector3(coord[0], coord[1], coord[2])
+    objects = ue.Object.FindObjectsOfType(ue.GameObject)
+    for parents in objects:
+        if parents.name == 'puck2':
+            parents.transform.position = vector
+            ue.Debug.Log(str(parents.transform.position))
+            #time.sleep(1)
+            count += 1
