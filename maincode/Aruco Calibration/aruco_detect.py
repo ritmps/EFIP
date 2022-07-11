b6639b459eb7b7e1f9167154bb365b33df1afaae
ramancini04 <bam3869@rit.edu>
2022-07-11 17:16:24 -0400

    updated aruco_detect.py

--- maincode/Aruco Calibration/aruco_detect.py
+++ maincode/Aruco Calibration/aruco_detect.py
@@ -1,10 +1,13 @@
 import argparse
+import time
 import imutils
 import cv2
 import numpy as np
 import sys
 
 global HOST, PORT, directory, tag_type
+
+verbose = True
 
 ARUCO_DICT = {
 	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
@@ -31,7 +34,7 @@ ARUCO_DICT = {
 }
 
 def args_parse():
-    global HOST, PORT, screenW, screenH
+    global HOST, PORT, directory, tag_type
     parser = argparse.ArgumentParser(description='Run GStreamer RTP stream')
     parser.add_argument('-i', '--host', type=str, default="0.0.0.0", help="Host's port\n    (Default: 0.0.0.0)")
     parser.add_argument('-p', '--port', type=int, default=5004, help="Host's port\n    (Default: 5004)")
@@ -43,3 +46,121 @@ def args_parse():
     directory = args.directory
     tag_type = ARUCO_DICT[args.type]
     print(f'Host: {HOST}\nPort: {PORT}')
+
+def gstreamer_in(width=1920, height=1080, fps=60):
+    pipeinParams = \
+        f"nvarguscamerasrc ! " \
+        f"video/x-raw(memory:NVMM), width={width}, height={height}, format=(string)NV12, framerate={fps}/1 ! " \
+        f"nvvidconv flip-method=0 ! "\
+        f"video/x-raw, width=1920, height=1080, format=(string)BGRx ! " \
+        f"videoconvert ! " \
+        f"video/x-raw, format=(string)BGR ! " \
+        f"appsink"
+    return (pipeinParams)
+
+# Define the output stream for gstreamer
+def gstreamer_out(host, port):
+    pipeoutParams = \
+        f"appsrc ! " \
+        f"video/x-raw, format=BGR ! " \
+        f"queue ! " \
+        f"videoconvert ! " \
+        f"video/x-raw,format=BGRx ! " \
+        f"nvvidconv ! " \
+        f"nvv4l2h264enc ! " \
+        f"h264parse ! " \
+        f"rtph264pay pt=96 config-interval=1 ! " \
+        f"udpsink host={host} port={port}"
+    return (pipeoutParams)
+
+def getDistortLUT():
+    # Read the csv file and store in 2D array
+    remap_lut = np.loadtxt('/home/fip/GitHub/EFIP/maincode/remap_lut.csv', delimiter=',', dtype=np.float32)
+    
+    # Split the array into mapx and mapy
+    mapx, mapy = np.vsplit(remap_lut, 2)
+
+    return (mapx, mapy)
+
+# Function to undistort the image using the look up table
+def undistort_img(image, lutmapx, lutmapy):
+    undist = cv2.remap(image, lutmapx, lutmapy, cv2.INTER_LINEAR)
+    return undist
+
+def detect_aruco(image):
+	# detect aruco tags
+	aruco_dict = tag_type
+	parameters = cv2.aruco.DetectorParameters_create()
+	corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
+	return corners, ids, rejectedImgPoints
+
+def read_cam():
+    global img, stop_thread
+
+    # First iteration of the while loop = true
+    firstloop = True
+
+    # Stop any running threads = false
+    stop_thread = False
+
+    # Read Gstreamer pipeline into OpenCV (pipein)
+    cap = cv2.VideoCapture(gstreamer_in(), cv2.CAP_GSTREAMER)
+
+    # Get the width, height and fps of the stream and print it to the console
+    w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print(f'\n--------------------------------------------------\n' \
+          f'Src opened, {w}x{h} @ {fps} fps' \
+          f'\n--------------------------------------------------\n')
+
+    # Create a look up table for distortion correction
+    lutmapx, lutmapy = getDistortLUT()
+
+    # Write OpenCV frames to Gstreamer stream (pipeout)
+    gst_out = gstreamer_out(host=HOST, port=PORT)
+    out = cv2.VideoWriter(gst_out, cv2.CAP_GSTREAMER, 0, float(fps), (int(w), int(h)))
+
+    if not out.isOpened():
+        print('\n--------------------------------------------------\n' \
+              'Failed to open output' \
+              '\n--------------------------------------------------\n')
+        exit()
+
+    if cap.isOpened():
+        # Give the camera time to warm up
+        time.sleep(2.0)
+        while True:
+            try:
+                ret_val, img = cap.read()
+
+                if not ret_val:
+                    break
+                
+                if verbose:
+                    undist_time_init = time.time()
+                # Undistort the image
+                undist_img = undistort_img(img, lutmapx, lutmapy)
+                if verbose:
+                    print(f'[INFO] Undistort time: {(time.time() - undist_time_init) * 1000} ms')
+
+                # if verbose:
+                #     track_time_init = time.time()
+                # # Image with the track
+                # track_img = color_track(undist_img)
+                # if verbose:
+                #     print(f'[INFO] Track time: {(time.time() - track_time_init) * 1000} ms')
+
+                out.write(undist_img)
+                cv2.waitKey(1)
+            except KeyboardInterrupt:
+                break
+    else:
+        print("pipeline open failed")
+
+    print("successfully exit")
+
+
+if __name__ == '__main__':
+    args_parse()
+    read_cam()
