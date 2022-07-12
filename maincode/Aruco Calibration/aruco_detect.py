8ea3fc043811376dd0e382e44356a55cf9a5a920
ramancini04 <bam3869@rit.edu>
2022-07-12 22:09:34 +0000

    added timers and detection

--- maincode/Aruco Calibration/aruco_detect.py
+++ maincode/Aruco Calibration/aruco_detect.py
@@ -5,9 +5,10 @@ import cv2
 import numpy as np
 import sys
 
-global HOST, PORT, directory, tag_type
+global HOST, PORT, lutPath, directory, tag_type
 
 verbose = True
+timings = True
 
 ARUCO_DICT = {
 	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
@@ -33,16 +34,55 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
 
+class timer:
+    global verbose, timings
+    
+    def __init__(self):
+        self.avg = 0
+        self.count = 0
+        self.start = 0
+        self.curDuration = 0
+    
+    def start_timer(self, action: str = None):
+        self.start = time.time()
+        if action is ((not None) and (verbose)):
+                print(f'[INFO] {action.upper()} STARTED')
+
+    def update(self, action: str):
+        if verbose or timings:
+            self.curDuration = (time.time() - self.start) * 1000
+            if verbose:
+                print(f'[INFO] {action.upper()} TOOK {self.curDuration}ms')
+            if timings:
+                self.update_average()
+
+    def update_average(self):
+        self.count += 1
+        self.avg = self.avg + ((self.curDuration - self.avg) / self.count)
+        self.start_timer()
+
+    def get_average(self):
+        return self.avg
+    
+    def get_curDuration(self):
+        return self.curDuration
+
+lutTime = timer()
+avgUndistTime = timer()
+avgDetectTime = timer()
+
 def args_parse():
-    global HOST, PORT, directory, tag_type
+    global HOST, PORT, lutPath, directory, tag_type
     parser = argparse.ArgumentParser(description='Run GStreamer RTP stream')
     parser.add_argument('-i', '--host', type=str, default="0.0.0.0", help="Host's port\n    (Default: 0.0.0.0)")
     parser.add_argument('-p', '--port', type=int, default=5004, help="Host's port\n    (Default: 5004)")
+    parser.add_argument('-l', '--lookup', type=str, default="../remap_lut.csv", help="Path to lookup table\n    (Default: /workspaces/EFIP/maincode/remap_lut.csv)")
     parser.add_argument('-d', '--directory', type=str, default="Tags", help="Directory to ArUCo tags\n    (Default: Tags)")
     parser.add_argument('-t', '--type', type=str, default="DICT_5X5_100", help="Type of ArUCo tag to detect\n    (Default: DICT_5X5_100)")
     args = parser.parse_args()
     HOST = args.host
     PORT = args.port
+    lutPath = args.lookup
     directory = args.directory
     tag_type = ARUCO_DICT[args.type]
     print(f'Host: {HOST}\nPort: {PORT}')
@@ -74,25 +114,56 @@ def gstreamer_out(host, port):
     return (pipeoutParams)
 
 def getDistortLUT():
+    global lutTime
+    lutTime.start_timer()
+    
+    print(f"[INFO] LOADING CALIBRATION LOOKUP TABLE: {lutPath}\n" \
+        f"[INFO] PLEASE BE PATIENT, THIS CAN TAKE A MOMENT...")
+
     # Read the csv file and store in 2D array
-    remap_lut = np.loadtxt('/home/fip/GitHub/EFIP/maincode/remap_lut.csv', delimiter=',', dtype=np.float32)
+    remap_lut = np.loadtxt(lutPath, delimiter=',', dtype=np.float32)
     
     # Split the array into mapx and mapy
     mapx, mapy = np.vsplit(remap_lut, 2)
 
+    lutTime.update('loading calibration lookup table')
     return (mapx, mapy)
 
 # Function to undistort the image using the look up table
 def undistort_img(image, lutmapx, lutmapy):
+    global avgUndistTime
+    avgUndistTime.start_timer('undistorting image')
+    
+    # Undistort the image using the look up table
     undist = cv2.remap(image, lutmapx, lutmapy, cv2.INTER_LINEAR)
+
+    avgUndistTime.update('undistorting image')
     return undist
 
 def detect_aruco(image):
-	# detect aruco tags
-	aruco_dict = tag_type
-	parameters = cv2.aruco.DetectorParameters_create()
-	corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
-	return corners, ids, rejectedImgPoints
+    global avgDetectTime
+    avgDetectTime.start_timer('detecting aruco tags')
+	
+    # detect aruco tags
+    parameters = cv2.aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, tag_type, parameters=parameters)
+    
+    avgDetectTime.update('detecting aruco tags')
+    return corners, ids, rejectedImgPoints
+
+# Returns corners, ids, rejected image points, and marks up an image using the detected points
+def detect_aruco_markup(image):
+    global avgDetectTime
+    avgDetectTime.start_timer('detecting aruco tags')
+    
+    # detect aruco tags
+    parameters = cv2.aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, tag_type, parameters=parameters)
+    # draw aruco tags
+    image = cv2.aruco.drawDetectedMarkers(image, corners, ids)
+
+    avgDetectTime.update('detecting aruco tags')
+    return image, corners, ids, rejectedImgPoints
 
 def read_cam():
     global img, stop_thread
@@ -137,21 +208,18 @@ def read_cam():
                 if not ret_val:
                     break
                 
-                if verbose:
-                    undist_time_init = time.time()
                 # Undistort the image
                 undist_img = undistort_img(img, lutmapx, lutmapy)
+
+                # Detect aruco tags
+                arucoimg, corners, ids, rejectedImgPoints = detect_aruco_markup(undist_img)
+                    
                 if verbose:
-                    print(f'[INFO] Undistort time: {(time.time() - undist_time_init) * 1000} ms')
+                    print(f'[INFO] Corners: {corners}\n' \
+                          f'[INFO] IDs: {ids}\n' \
+                          f'[INFO] Rejected image points: {rejectedImgPoints}')
 
-                # if verbose:
-                #     track_time_init = time.time()
-                # # Image with the track
-                # track_img = color_track(undist_img)
-                # if verbose:
-                #     print(f'[INFO] Track time: {(time.time() - track_time_init) * 1000} ms')
-
-                out.write(undist_img)
+                out.write(arucoimg)
                 cv2.waitKey(1)
             except KeyboardInterrupt:
                 break
